fd5f9ad86167a74a6b7ce7abf27bbc436e8bc827
Evan Kiser <evan.kiser@bsu.edu>
2023-11-07 12:41:37 -0500

    ✅ Updated RomanNumeralConverter to pass test for numbers 40 and 50

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,8 +1,8 @@
 public class RomanNumeralConverter {
 
     StringBuilder romanNumeralHolder = new StringBuilder();
-    int[] sigNumbers = {10, 9, 5, 4, 1};
-    String[] romanNumerals = {"X", "IX", "V", "IV", "I"};
+    int[] sigNumbers = {50, 40, 10, 9, 5, 4, 1};
+    String[] romanNumerals = {"L", "XL", "X", "IX", "V", "IV", "I"};
     public String convert(int userDigit) {
         for( int i=0; i<sigNumbers.length; i++) {
             while ( userDigit >= sigNumbers[i]) {
