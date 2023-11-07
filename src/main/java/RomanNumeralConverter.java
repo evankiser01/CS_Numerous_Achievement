4e7ad7a41df4753dba188640716e7e3d6cf46e94
Evan Kiser <evan.kiser@bsu.edu>
2023-11-07 12:32:35 -0500

    ✅ Updated RomanNumeralConverter to pass test 6-10

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,8 +1,8 @@
 public class RomanNumeralConverter {
 
     StringBuilder romanNumeralHolder = new StringBuilder();
-    int[] sigNumbers = {5, 4, 1};
-    String[] romanNumerals = {"V", "IV", "I"};
+    int[] sigNumbers = {10, 9, 5, 4, 1};
+    String[] romanNumerals = {"X", "IX", "V", "IV", "I"};
     public String convert(int userDigit) {
         for( int i=0; i<sigNumbers.length; i++) {
             while ( userDigit >= sigNumbers[i]) {
