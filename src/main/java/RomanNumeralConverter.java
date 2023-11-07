b877d3c0fbae8139a5ceb4ee3e845681970ff1b5
Evan Kiser <evan.kiser@bsu.edu>
2023-11-07 13:03:34 -0500

    ✅ Updated RomanNumeralConverter to pass test for number 100

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,8 +1,8 @@
 public class RomanNumeralConverter {
 
     StringBuilder romanNumeralHolder = new StringBuilder();
-    int[] sigNumbers = {90, 50, 40, 10, 9, 5, 4, 1};
-    String[] romanNumerals = {"XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int[] sigNumbers = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] romanNumerals = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     public String convert(int userDigit) {
         for( int i=0; i<sigNumbers.length; i++) {
             while ( userDigit >= sigNumbers[i]) {
