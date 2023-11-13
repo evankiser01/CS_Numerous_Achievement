975602056d502c881371ee5059c0cd1b08f03884
Evan Kiser <evan.kiser@bsu.edu>
2023-11-13 15:15:40 -0500

    ✅ Updated RomanNumeralConverter to pass test for number 1000

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,8 +1,8 @@
 public class RomanNumeralConverter {
 
     StringBuilder romanNumeralHolder = new StringBuilder();
-    int[] sigNumbers = {900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    String[] romanNumerals = {"CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int[] sigNumbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     public String convert(int userDigit) {
         for( int i=0; i<sigNumbers.length; i++) {
             while ( userDigit >= sigNumbers[i]) {
